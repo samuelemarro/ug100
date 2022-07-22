353c4085c0432c998c1f5d8f83f044a9856fa0c6
Samuele Marro <marrosamuele@gmail.com>
2022-07-22 14:24:31 +0200

    Documented.

--- src/ug100/dataset.py
+++ src/ug100/dataset.py
@@ -64,6 +64,7 @@ def _filter_attacks(data, attacks):
         }
 
 class UG100Base(Dataset):
+    """Base class for UG100 datasets."""
     def __init__(self, dataset : str, architecture : str, training_type : str, path : str, root : Union[str, Path], download : bool, url : str, extraction_path : Union[str, Path]):
         super().__init__()
         path = Path(path)
@@ -124,6 +125,22 @@ class UG100ApproximateAdversarial(UG100Base):
             download : bool = True,
             transform : Optional[Callable] = None
         ) -> None:
+        """Loads a dataset containing the approximate adversarial distances.
+        If attacks is a list, the adversarial examples are stored as attack name
+        - adversarial example dictionaries. If attacks is a single element, the
+        adversarial examples are stored directly.
+
+        Args:
+            dataset (str, optional): target dataset. Options: ['mnist', 'cifar10']. Defaults to 'mnist'.
+            architecture (str, optional): target architecture. Options: ['a', 'b', 'c']. Defaults to 'a'.
+            training_type (str, optional): target training type. Options: ['standard', 'adversarial', 'relu']. Defaults to 'standard'.
+            parameter_set: (str, optional): parameter set of the attacks. Options: ['strong', 'balanced']. Defaults to 'strong'.
+            attacks : (Union[str, List[str]], optional): list of chosen attacks.
+                Valid attacks: ['bim', 'brendel', 'carlini', 'deepfool' 'fast_gradient', 'pgd', 'uniform']. Defaults to all of them.
+            root (Union[str, Path], optional): root of the data folder. Defaults to './data/ug100'.
+            download (bool, optional): whether to download the dataset, if not present. Defaults to True.
+            transform (Optional[Callable], optional): transformation to be applied to adversarial examples. Defaults to None.
+        """
         if parameter_set not in ['balanced', 'strong']:
             raise ValueError(f'Unsupported training type "{training_type}".')
         if training_type not in ['standard', 'adversarial', 'relu']:
@@ -146,7 +163,10 @@ class UG100ApproximateAdversarial(UG100Base):
     def __getitem__(self, index) -> Union[Dict[str, torch.Tensor], torch.Tensor]:
         element = super().__getitem__(index)
         if self.transform:
-            return {key: self.transform(value) for key, value in element.items()}
+            if isinstance(element, dict):
+                return {key: self.transform(value) for key, value in element.items()}
+            else:
+                return self.transform(element)
         else:
             return element
 
@@ -160,6 +180,16 @@ class UG100MIPAdversarial(UG100Base):
             download : bool = True,
             transform : Optional[Callable] = None
         ) -> None:
+        """Loads a dataset containing the MIP adversarial examples.
+
+        Args:
+            dataset (str, optional): target dataset. Options: ['mnist', 'cifar10']. Defaults to 'mnist'.
+            architecture (str, optional): target architecture. Options: ['a', 'b', 'c']. Defaults to 'a'.
+            training_type (str, optional): target training type. Options: ['standard', 'adversarial', 'relu']. Defaults to 'standard'.
+            root (Union[str, Path], optional): root of the data folder. Defaults to './data/ug100'.
+            download (bool, optional): whether to download the dataset, if not present. Defaults to True.
+            transform (Optional[Callable], optional): transformation to be applied to adversarial examples. Defaults to None.
+        """
         path = Path(root) / 'adversarials' / 'mip' / dataset / architecture / (training_type + '.pt')
         extraction_path = Path(root) / 'adversarials' / 'mip' / dataset
         url = URL_BASE + f'adversarials_mip_{dataset}.zip'
@@ -180,10 +210,25 @@ class UG100ApproximateDistance(UG100Base):
             architecture : str = 'a',
             training_type : str = 'standard',
             parameter_set : str = 'strong',
-            attacks : List[str] = ATTACKS,
+            attacks : Union[str, List[str]] = ATTACKS,
             root : Union[str, Path] = './data/ug100',
             download : bool = True
         ) -> None:
+        """Loads a dataset containing the approximate adversarial examples.
+        If attacks is a list, the adversarial examples are stored as attack name
+        - adversarial example dictionaries. If attacks is a single element, the
+        adversarial examples are stored directly.
+
+        Args:
+            dataset (str, optional): target dataset. Options: ['mnist', 'cifar10']. Defaults to 'mnist'.
+            architecture (str, optional): target architecture. Options: ['a', 'b', 'c']. Defaults to 'a'.
+            training_type (str, optional): target training type. Options: ['standard', 'adversarial', 'relu']. Defaults to 'standard'.
+            parameter_set: (str, optional): parameter set of the attacks. Options: ['strong', 'balanced']. Defaults to 'strong'.
+            attacks : (Union[str, List[str]], optional): list of chosen attacks.
+                Valid attacks: ['bim', 'brendel', 'carlini', 'deepfool' 'fast_gradient', 'pgd', 'uniform']. Defaults to all of them.
+            root (Union[str, Path], optional): root of the data folder. Defaults to './data/ug100'.
+            download (bool, optional): whether to download the dataset, if not present. Defaults to True.
+        """
         if parameter_set not in ['balanced', 'strong']:
             raise ValueError(f'Unsupported training type "{training_type}".')
         if training_type not in ['standard', 'adversarial', 'relu']:
@@ -212,6 +257,15 @@ class UG100MIPBounds(UG100Base):
             root : Union[str, Path] = './data/ug100',
             download : bool = True
         ) -> None:
+        """Loads a dataset containing the MIP convergence bounds.
+
+        Args:
+            dataset (str, optional): target dataset. Options: ['mnist', 'cifar10']. Defaults to 'mnist'.
+            architecture (str, optional): target architecture. Options: ['a', 'b', 'c']. Defaults to 'a'.
+            training_type (str, optional): target training type. Options: ['standard', 'adversarial', 'relu']. Defaults to 'standard'.
+            root (Union[str, Path], optional): root of the data folder. Defaults to './data/ug100'.
+            download (bool, optional): whether to download the dataset, if not present. Defaults to True.
+        """
         path = Path(root) / 'distances' / 'mip' / dataset / architecture / (training_type + '.json')
         extraction_path = Path(root) / 'distances'
         url = URL_BASE + 'distances.zip'
@@ -228,6 +282,15 @@ class UG100MIPTime(UG100Base):
             root : Union[str, Path] = './data/ug100',
             download : bool = True
         ) -> None:
+        """Loads a dataset containing the MIP convergence times.
+
+        Args:
+            dataset (str, optional): target dataset. Options: ['mnist', 'cifar10']. Defaults to 'mnist'.
+            architecture (str, optional): target architecture. Options: ['a', 'b', 'c']. Defaults to 'a'.
+            training_type (str, optional): target training type. Options: ['standard', 'adversarial', 'relu']. Defaults to 'standard'.
+            root (Union[str, Path], optional): root of the data folder. Defaults to './data/ug100'.
+            download (bool, optional): whether to download the dataset, if not present. Defaults to True.
+        """
         path = Path(root) / 'mip_times' / dataset / architecture / (training_type + '.json')
         extraction_path = Path(root) / 'mip_times'
         url = URL_BASE + 'mip_times.zip'
@@ -237,6 +300,7 @@ class UG100MIPTime(UG100Base):
         return super().__getitem__(index)
 
 class IndexedDataset(Dataset):
+    """Allows indexing of a sparse dataset in a dense manner."""
     def __init__(self, base_dataset : UG100Base) -> None:
         super().__init__()
         self.base_dataset = base_dataset
@@ -248,15 +312,14 @@ class IndexedDataset(Dataset):
     def __getitem__(self, index):
         return super().__getitem__(self.ordered_keys[index])
 
-class MergedDataset(Dataset):
+class MultiDataset(Dataset):
+    """A dataset that returns the elements of multiple datasets."""
     def __init__(self, *datasets) -> None:
         super().__init__()
 
         if len(datasets) == 0:
             raise ValueError('At least one dataset required.')
 
-        if not all(len(dataset) == len(datasets[0]) for dataset in datasets):
-            raise ValueError('Datasets must have the same length.')
         self.datasets = datasets
     
     def __len__(self):
