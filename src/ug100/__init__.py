79b393a104aace82ef0b4e046db571b319ddef30
Samuele Marro <marrosamuele@gmail.com>
2022-07-22 18:13:27 +0200

    Fixed __init__.py.

--- src/ug100/__init__.py
+++ src/ug100/__init__.py
@@ -1,1 +1,4 @@
-from dataset import UG100Base, UG100ApproximateAdversarial, UG100ApproximateDistance, UG100MIPAdversarial, UG100MIPBounds, UG100MIPTime, ATTACKS
+from .dataset import \
+    UG100Base, UG100ApproximateAdversarial, UG100ApproximateDistance, \
+    UG100MIPAdversarial, UG100MIPBounds, UG100MIPTime, IndexedDataset, \
+    MultiDataset, ATTACKS
